b79867a348189243d06272e46ea8a18d4d3f807c
Aliza Chazan <learnalgoritmit@gmail.com>
2025-07-18 06:20:22 +0300

    chore: remove unused variables from locale booking and checkout pages (lint clean)

--- src/app/[locale]/booking/page.tsx
+++ src/app/[locale]/booking/page.tsx
@@ -1,11 +1,8 @@
 "use client";
 import BookingForm from "@/components/BookingForm";
 import BackgroundWrapper from "@/components/BackgroundWrapper";
-import { useParams } from "next/navigation";
 
 export default function LocaleBookingPage() {
-  const params = useParams();
-  const locale = params.locale || "en";
   return (
     <BackgroundWrapper>
       <BookingForm />
--- src/app/[locale]/checkout/page.tsx
+++ src/app/[locale]/checkout/page.tsx
@@ -1,11 +1,8 @@
 "use client";
 import CheckoutSummary from "@/components/CheckoutSummary";
 import BackgroundWrapper from "@/components/BackgroundWrapper";
-import { useParams } from "next/navigation";
 
 export default function LocaleCheckoutPage() {
-  const params = useParams();
-  const locale = params.locale || "en";
   return (
     <BackgroundWrapper>
       <CheckoutSummary />
